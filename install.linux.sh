9381f37ab2d4a90b9fcac71f6d1b95cf3ec72df3
ash <the0demiurge@yahoo.com>
2016-10-12 14:24:49 +0800

    Update install.linux.sh

--- install.linux.sh
+++ install.linux.sh
@@ -3,6 +3,7 @@ echo 'installing the linux scripts'
 echo 'make sure that you are not in use of root user'
 sudo cp -p ./linux/etc/* /etc/
 cp -p ./linux/ash/* ~/
+sudo sh /etc/cron.daily/update-hosts
 echo 'this script is only for my own, so you should edit the destination and
 /etc/backup-scripts.sh
 ~/.shell.d/*

252aeb33d2c6e0659947cb2484e9a89dbb783492
the0demiurge <the0demiurge@gmail.com>
2017-04-15 15:59:19 +0800

    安装脚本完成

--- install-linux.sh
+++ install-linux.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 echo 'installing the linux scripts'
-echo 'make sure that you are not in use of root user'
+./install-hosts.sh
 sudo cp -p ./linux/etc/* /etc/
 cp -p ./linux/ash/* ~/
 sudo add-apt-repository $(cat ~/.backup/ppa)
+sudo apt update
 sudo apt install $(cat ~/.backup/apps/ubuntu.list)
 python3 ~/.userscripts/install.py
-echo 'this script is only for my own, so you should edit the destination and
-/etc/backup-scripts.sh
-~/.shell.d/*
 chsh -s /usr/bin/fish
-and so on, yourself.'
+echo 'all done!'

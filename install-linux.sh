9b5e7c3320ed9a4eacc468d2aedabdd80ae0b11e
the0demiurge <the0demiurge@gmail.com>
2017-04-15 15:46:05 +0800

    changed a lot for omf

--- install-linux.sh
+++ install-linux.sh
@@ -3,9 +3,11 @@ echo 'installing the linux scripts'
 echo 'make sure that you are not in use of root user'
 sudo cp -p ./linux/etc/* /etc/
 cp -p ./linux/ash/* ~/
-sudo sh /etc/cron.daily/update-hosts
+sudo add-apt-repository $(cat ~/.backup/ppa)
+sudo apt install $(cat ~/.backup/apps/ubuntu.list)
+python3 ~/.userscripts/install.py
 echo 'this script is only for my own, so you should edit the destination and
 /etc/backup-scripts.sh
 ~/.shell.d/*
-~/.zshrc
+chsh -s /usr/bin/fish
 and so on, yourself.'

b642b89a3cff5bcb6fea565c329a9809a98cdc09
the0demiurge <the0demiurge@gmail.com>
2017-04-20 16:40:43 +0800

    changed the install scripts, added neovim and changed aix-vim to spacevim

--- install-linux.sh
+++ install-linux.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 echo 'installing the linux scripts'
 ./install-hosts.sh
-sudo cp -p ./linux/etc/* /etc/
+sudo cp -p ./linux/etc/cron.daily/update-hosts /etc/cron.daily/
+sudo cp -p ./linux/etc/hosts.add /etc/
+sudo cp -p ./linux/etc/hosts.list /etc/
+sudo cp -p ./linux/etc/cron.hourly/wallpaper-earthview /etc/cron.hourly/
+
 cp -p ./linux/ash/* ~/
 sudo add-apt-repository $(cat ~/.backup/ppa)
 sudo apt update
@@ -9,3 +13,4 @@ sudo apt install $(cat ~/.backup/apps/ubuntu.list)
 python3 ~/.userscripts/install.py
 chsh -s /usr/bin/fish
 echo 'all done!'
+echo 'please manually edit and place ./linux/etc/cron.daily/backup-scripts to /etc/cron.daily/'

6c9470a61934559e90d2a98a1242f53a91aa1a6b
the0demiurge <the0demiurge@gmail.com>
2017-04-20 18:43:13 +0800

    optimized for install scripts

--- install-hosts.sh
+++ install-hosts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo cp ./linux/etc/cron.daily/update-hosts /etc/cron.daily
 sudo cp ./linux/etc/hosts.list /etc/
+sudo cp ./linux/etc/hosts.add /etc/
 sudo sh /etc/cron.daily/update-hosts
 
--- install-linux.sh
+++ install-linux.sh
@@ -1,17 +1,53 @@
 #!/bin/sh
 echo 'installing the linux scripts'
-./install-hosts.sh
-sudo cp -p ./linux/etc/cron.daily/update-hosts /etc/cron.daily/
-sudo cp -p ./linux/etc/hosts.add /etc/
-sudo cp -p ./linux/etc/hosts.list /etc/
-sudo cp -p ./linux/etc/cron.hourly/wallpaper-earthview /etc/cron.hourly/
 
-cp -p ./linux/ash/* ~/
+read -p 'install hosts?[Y/n]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
+    ./install-hosts.sh
+fi
+
+read -p 'install cronjobs(wallpaper-earthview updater for Gnome3)?[Y/n]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
+    sudo cp -p ./linux/etc/cron.hourly/wallpaper-earthview /etc/cron.hourly/
+fi
+
+read -p 'install backuping cronjob for installed scripts?[y/N]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='n';fi
+if [ '$PROMPT'=='y' ];then
+    cp -p ./linux/etc/cron.daily/backup-scripts /etc/cron.daily/
+    echo 'edit the /etc/cron.daily/backup-scripts!'
+    sleep 5
+    vi /etc/cron.daily/backup-scripts
+fi
+
+read -p 'install user configs and userscripts?[Y/n]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
+    cp -p ./linux/ash/* ~/
+fi
+
+read -p 'Updating ppas?[Y/n]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
 sudo add-apt-repository $(cat ~/.backup/ppa)
 sudo apt update
+fi
+
+read -p 'install my installed apps?[Y/n]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
 sudo apt install $(cat ~/.backup/apps/ubuntu.list)
+fi
+
+read -p 'install and config softwares including docker, powerline, omf?[y/N]:' PROMPT
+if [ -z $PROMPT ];then PROMPT='y';fi
+if [ '$PROMPT'=='y' ];then
 sudo apt install -y python3
 python3 ~/.userscripts/.init/install.py
+fi
+
 chsh -s /usr/bin/fish
 echo 'all done!'
 echo 'please manually edit and place ./linux/etc/cron.daily/backup-scripts to /etc/cron.daily/'

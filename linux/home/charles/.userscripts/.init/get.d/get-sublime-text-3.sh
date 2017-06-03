65156c1fa2664ceb2b5a5b22e40d044a3bfb7319
Charles Xu <charles@Laptop>
2017-06-03 22:06:06 +0800

    System re-installed, and lot's of config changed

---
+++ linux/home/charles/.userscripts/.init/get.d/get-sublime-text-3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text

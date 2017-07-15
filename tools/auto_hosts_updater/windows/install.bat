1eb517f6a80917a0423f2e0c9df7384f2fc30eb1
Charles Xu <charl3s.xu@gmail.com>
2017-07-15 21:50:28 +0800

    更新了hosts地址，需要重新安装hosts脚本（直接覆盖安装即可）原来的一直上不去网，hosts失效很久了

--- tools/auto_hosts_updater/windows/install.bat
+++ tools/auto_hosts_updater/windows/install.bat
@@ -6,7 +6,7 @@ echo mkdir %TEMP%\hosts > %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo $client = new-object System.Net.WebClient >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 rem echo $client.DownloadFile('https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt', '%TEMP%\hosts\hosts2') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 rem echo $client.DownloadFile('https://raw.githubusercontent.com/racaljk/hosts/master/hosts', '%TEMP%\hosts\hosts3') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+echo $client.DownloadFile('https://raw.githubusercontent.com/racaljk/hosts/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts-ad') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo cat %TEMP%\hosts\* ^> %SystemRoot%\System32\drivers\etc\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo rm -recurse %TEMP%\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1

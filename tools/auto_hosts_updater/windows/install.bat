ada975082ab7740df012d73ff93122338538ef26
Charles Xu <charl3s.xu@gmail.com>
2017-09-03 15:43:28 +0800

    Go Hosts似乎无法顺利上Google，切换为了之前的

--- tools/auto_hosts_updater/windows/install.bat
+++ tools/auto_hosts_updater/windows/install.bat
@@ -4,10 +4,10 @@ schtasks /create /tn update-hosts /sc daily /ru "NT Authority\System" /tr %Syste
 #update-hosts.ps1
 echo mkdir %TEMP%\hosts > %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo $client = new-object System.Net.WebClient >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-rem echo $client.DownloadFile('https://github.com/googlehosts/hosts/raw/master/hosts-files/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-rem echo $client.DownloadFile('https://github.com/vokins/yhosts/raw/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts-ad') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+echo $client.DownloadFile('https://github.com/googlehosts/hosts/raw/master/hosts-files/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+echo $client.DownloadFile('https://github.com/vokins/yhosts/raw/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+rem echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+rem echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts-ad') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo cat %TEMP%\hosts\* ^> %SystemRoot%\System32\drivers\etc\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo rm -recurse %TEMP%\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 

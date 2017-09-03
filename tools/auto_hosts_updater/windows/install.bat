498d08fcc60988fe51b26617e5f8b0b21135e715
Charles Xu <charl3s.xu@gmail.com>
2017-09-03 15:05:59 +0800

    hosts_updater 更新了源

--- tools/auto_hosts_updater/windows/install.bat
+++ tools/auto_hosts_updater/windows/install.bat
@@ -4,10 +4,8 @@ schtasks /create /tn update-hosts /sc daily /ru "NT Authority\System" /tr %Syste
 #update-hosts.ps1
 echo mkdir %TEMP%\hosts > %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo $client = new-object System.Net.WebClient >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-rem echo $client.DownloadFile('https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt', '%TEMP%\hosts\hosts2') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-rem echo $client.DownloadFile('https://raw.githubusercontent.com/racaljk/hosts/master/hosts', '%TEMP%\hosts\hosts3') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-echo $client.DownloadFile('https://raw.githubusercontent.com/racaljk/hosts/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
-echo $client.DownloadFile('https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts-ad') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+echo $client.DownloadFile('https://github.com/googlehosts/hosts/raw/master/hosts-files/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
+echo $client.DownloadFile('https://github.com/vokins/yhosts/raw/master/hosts') >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo cat %TEMP%\hosts\* ^> %SystemRoot%\System32\drivers\etc\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 echo rm -recurse %TEMP%\hosts >> %SystemRoot%\System32\drivers\etc\update-hosts.ps1
 

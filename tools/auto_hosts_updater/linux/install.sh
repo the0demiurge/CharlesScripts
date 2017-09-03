8305e7eca5318f7cc3e6104b8bb9476c9d8caae3
Charles Xu <charl3s.xu@gmail.com>
2017-09-03 15:05:59 +0800

    hosts_updater 更新了源

--- tools/auto_hosts_updater/linux/install.sh
+++ tools/auto_hosts_updater/linux/install.sh
@@ -4,16 +4,14 @@ echo '#!/bin/bash'|sudo tee /etc/cron.daily/update-hosts
 echo 'Automatic Hosts Updater'|sudo tee -a /etc/cron.daily/update-hosts
 echo ''|sudo tee -a /etc/cron.daily/update-hosts
 echo 'HOSTS_LIST=('|sudo tee -a /etc/cron.daily/update-hosts
-# echo '"https://raw.githubusercontent.com/racaljk/hosts/master/hosts"'|sudo tee -a /etc/cron.daily/update-hosts
-# echo '"https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt"'|sudo tee -a /etc/cron.daily/update-hosts
-echo '"https://raw.githubusercontent.com/racaljk/hosts/master/hosts"'|sudo tee -a /etc/cron.daily/update-hosts
-echo '"https://raw.githubusercontent.com/Lerist/Go-Hosts/master/hosts-ad"'|sudo tee -a /etc/cron.daily/update-hosts
+echo '"https://github.com/googlehosts/hosts/raw/master/hosts-files/hosts"'|sudo tee -a /etc/cron.daily/update-hosts
+echo '"https://github.com/vokins/yhosts/raw/master/hosts"'|sudo tee -a /etc/cron.daily/update-hosts
 echo ')'|sudo tee -a /etc/cron.daily/update-hosts
 echo ''|sudo tee -a /etc/cron.daily/update-hosts
 echo 'for item in ${HOSTS_LIST[@]};do'|sudo tee -a /etc/cron.daily/update-hosts
 echo '    false'|sudo tee -a /etc/cron.daily/update-hosts
 echo '    while [ $? != 0 ]; do'|sudo tee -a /etc/cron.daily/update-hosts
-echo '        curl $item|tee -a /tmp/hosts'|sudo tee -a /etc/cron.daily/update-hosts
+echo '        curl -L $item|tee -a /tmp/hosts'|sudo tee -a /etc/cron.daily/update-hosts
 echo '    done'|sudo tee -a /etc/cron.daily/update-hosts
 echo 'done'|sudo tee -a /etc/cron.daily/update-hosts
 echo ''|sudo tee -a /etc/cron.daily/update-hosts

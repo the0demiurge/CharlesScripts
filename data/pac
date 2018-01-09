7201b93119ba29dcf7aac7e874e4714c5431bba5
Charles Xu <charl3s.xu@gmail.com>
2018-01-09 16:19:57 +0800

    deleted history blobs

---
+++ data/pac
@@ -0,0 +1,10 @@
+http://127.0.0.1:16823/proxy_on.pac?1464244398546034791  #lantern
+http://ubdns.wlmacao.tk/proxy.pac  #unblock youku
+DNS IP地址：192.241.222.103
+DNS IPv6地址：2001:470:1f04:c8b::2
+DNS端口： 53,5353 [TCP+UDP]
+PAC 脚本地址： http://pac.uku.im/pac.pac
+PAC 脚本的地址：http://yo.uku.im/proxy.pac
+新版PAC脚本测试地址：http://ubdns.wlmacao.tk/proxy.pac
+非常规端口的PAC脚本地址：http://ubdns.wlmacao.tk/altproxy.pac
+东北大学图书馆wifi：SSID=neulib;proxy=202.118.8.13:3128
---
+++ test/install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+if [[ $0 != 'debug' ]]; then
+    set -e
+fi
+
+promptn(){
+    read -p 'Will you run '$1'?[y/N]' PMT
+    if [[ $PMT == 'y' || $PMT == 'Y' ]]; then
+        . ./$1
+    fi
+}
+
+prompty(){
+    read -p 'Will you run '$1'?[Y/n]' PMT
+    if [[ $PMT != 'n' || $PMT != 'N' ]]; then
+        . ./$1
+    fi
+}
+
+dependency(){
+    echo 'Installing part of the dependencies...'
+    sudo apt update
+    sudo apt install git espeak cowsay oneko sl fortune fish mlocate
+    sudo updatedb
+}
+
+gitclone(){
+    if [ ! -d ~/.local/share ]; then mkdir -p ~/.local/share; fi
+    git clone https://github.com/the0demiurge/CharlesScripts.git ~/.local/share/CharlesScripts
+    read -p 'Please type your backup git repo address. If you do not have one, you may create it on GitHub.com.\nPress Enter to skip\n' REPO
+    git clone $REPO $CHARLES_BACKUP||true
+    if [[ ! -x $CHARLES_BACKUP ]]; then
+        echo 'Clone failed! Default CharlesBackup will be cloned!'
+        git clone https://github.com/the0demiurge/CharlesScripts.git $CHARLES_BACKUP
+        echo 'You may modify $CHARLES_BACKUP, and type:'
+        echo 'cd $CHARLES_BACKUP'
+        echo 'git remote set-url origin <your-git-url>'
+        echo 'git add -A; git commit -m "init commit";git push -u origin master'
+    fi
+}
+
+
+get(){
+    Y_LIST=(get-fasd
+            get-powerline)
+            #get-oh-my-fish
+            # get-thefuck)
+
+    # N_LIST=(get-calibre
+            # get-docker
+            # get-spacemacs
+            # get-spacevim
+            # get-sublime-text-3
+            # get-xsh)
+
+    
+    for Y in ${Y_LIST[@]}; do
+        cd ~/.local/share/CharlesScripts/charles/installation.d/get.d/
+        prompty $Y
+    done
+    
+    for N in ${N_LIST[@]}; do
+        cd ~/.local/share/CharlesScripts/charles/installation.d/get.d/
+        prompty $N
+    done
+}
+
+conf(){
+    Y_LIST=(config-bash
+            # config-fish
+            config-git
+            config-powerline-bash
+            config-tmux)
+
+    N_LIST=(config-powerline-ipython)
+
+    
+    for Y in ${Y_LIST[@]}; do
+        cd ~/.local/share/CharlesScripts/charles/installation.d/conf.d/
+        echo $Y
+        prompty $Y
+    done
+    
+    for N in ${N_LIST[@]}; do
+        cd ~/.local/share/CharlesScripts/charles/installation.d/conf.d/
+        echo $N
+        prompty $N
+    done
+}
+
+restore(){
+    read -p 'DANGER! Restore dotfiles from ~/.local/share/CharlesScripts/data/home/.* ?[y/N]' PMT
+    if [[ $PMT == 'y' || $PMT == 'Y' ]]; then
+        cp -rvi ~/.local/share/CharlesScripts/data/home/.* ~/||true
+    fi
+    
+    cd ~/.local/share/CharlesScripts/charles/bin
+    
+    # Y_LIST=(
+    #         omf-restore)
+    
+    for Y in ${Y_LIST[@]}; do
+        echo $Y
+        prompty $Y
+    done
+}
+dependency
+gitclone
+get
+conf
+restore
+
+cd ~/.local/share/CharlesScripts/charles/installation.d/get.d/
+prompty get-oh-my-fish

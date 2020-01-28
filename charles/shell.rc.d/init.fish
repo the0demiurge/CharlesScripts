f07d86e9ffedaee4fe8048bbcf38e879d8a5a349
Charles <charl3s.xu@gmail.com>
2020-01-29 00:00:23 +0800

    .

--- charles/shell.rc.d/init.fish
+++ charles/shell.rc.d/init.fish
@@ -2,30 +2,30 @@ set -g CHARLES_HOME $HOME/.local/share/CharlesScripts/charles
 set -g CHARLES_BACKUP $HOME/.config/CharlesBackup
 
 #PATHES
-set -x fish_user_paths $CHARLES_HOME/bin $fish_user_paths
-set -x fish_user_paths $HOME/.local/bin $fish_user_paths
+set -g fish_user_paths $CHARLES_HOME/bin $fish_user_paths
+set -g fish_user_paths $HOME/.local/bin $fish_user_paths
 
 #VARIABLES
-set -x TF_CPP_MIN_LOG_LEVEL 2
-set -x EDITOR nvim
-set -x LC_ALL en_US.UTF-8
-set -x LANG en_US.UTF-8
-set -x FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(subl {})+abort'"
-set -x SDCV_PAGER less
+set -g TF_CPP_MIN_LOG_LEVEL 2
+set -g EDITOR nvim
+set -g LC_ALL en_US.UTF-8
+set -g LANG en_US.UTF-8
+set -g FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(subl {})+abort'"
+set -g SDCV_PAGER less
 
 #COLOR_LESS
-set -x PAGER "less"
-set -x LESS "-R -i -g -c -W"
-set -x LESSOPEN '|/usr/bin/env lesspipe.sh %s'
-set -x LESSCLOSE '/usr/bin/env lesspipe.sh %s %s'
+set -g PAGER "less"
+set -g LESS "-R -i -g -c -W"
+set -g LESSOPEN '|/usr/bin/env lesspipe.sh %s'
+set -g LESSCLOSE '/usr/bin/env lesspipe.sh %s %s'
 #COLOR MAN
-set -x LESS_TERMCAP_mb (printf "\033[01;31m")
-set -x LESS_TERMCAP_md (printf "\033[01;31m")
-set -x LESS_TERMCAP_me (printf "\033[0m")
-set -x LESS_TERMCAP_se (printf "\033[0m")
-set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
-set -x LESS_TERMCAP_ue (printf "\033[0m")
-set -x LESS_TERMCAP_us (printf "\033[01;32m")
+set -g LESS_TERMCAP_mb (printf "\033[01;31m")
+set -g LESS_TERMCAP_md (printf "\033[01;31m")
+set -g LESS_TERMCAP_me (printf "\033[0m")
+set -g LESS_TERMCAP_se (printf "\033[0m")
+set -g LESS_TERMCAP_so (printf "\033[01;44;33m")
+set -g LESS_TERMCAP_ue (printf "\033[0m")
+set -g LESS_TERMCAP_us (printf "\033[01;32m")
 
 #BOBTHEFISH
 set -g theme_date_format "+%A, %B %d, %l:%M%P"
@@ -50,32 +50,32 @@ function cls
     ls
 end
 
-function g
-    git add -A
-    git diff --cached
-    echo "type the commits or cancel with Ctrl+C"
-    git status
-    read COMMIT
-    git commit -m "$argv $COMMIT"
-    git log -1 HEAD
-    if git push
-        echo push succeeded
-        sl -e | lolcat
-        for i in (seq 25)
-            echo
-        end
-        fortune | cowsay -f duck
-    else
-        for i in (seq 3)
-            echo push failed
-        end
-        echo push failed $PWD
-        for i in (seq 25)
-            echo
-        end
-        cowsay -f bong "push failed!!! push again!!!"
-    end
-end
+# function g
+#     git add -A
+#     git diff --cached
+#     echo "type the commits or cancel with Ctrl+C"
+#     git status
+#     read COMMIT
+#     git commit -m "$argv $COMMIT"
+#     git log -1 HEAD
+#     if git push
+#         echo push succeeded
+#         sl -e | lolcat
+#         for i in (seq 25)
+#             echo
+#         end
+#         fortune | cowsay -f duck
+#     else
+#         for i in (seq 3)
+#             echo push failed
+#         end
+#         echo push failed $PWD
+#         for i in (seq 25)
+#             echo
+#         end
+#         cowsay -f bong "push failed!!! push again!!!"
+#     end
+# end
 
 function mcd
     mkdir -p "$argv"
